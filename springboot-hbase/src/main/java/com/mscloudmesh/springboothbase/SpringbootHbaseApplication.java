4ec003cbc3e9a3f5d71092ffcaa5abae5ba09a21
活在当下201903 <328730659@qq.com>
2019-12-18 12:50:31 +0800

    hbase操作

---
+++ springboot-hbase/src/main/java/com/mscloudmesh/springboothbase/SpringbootHbaseApplication.java
@@ -0,0 +1,17 @@
+package com.mscloudmesh.springboothbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author kevin
+ * @date 2019/12/18
+ */
+@SpringBootApplication
+public class SpringbootHbaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootHbaseApplication.class, args);
+	}
+
+}
