44762d41b383bb6e794d144d65e5cef4251d7752
活在当下201903 <328730659@qq.com>
2019-12-22 13:38:08 +0800

    分布式事务lcn‘ ’

---
+++ springboot-dubbo-lcn/service-c/src/main/java/com/mscloudmesh/servicec/DemoMapper.java
@@ -0,0 +1,14 @@
+import com.mscloudmesh.common.db.mapper.BaseDemoMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+
+/**
+ * Description:
+ * Company: CodingApi
+ * Date: 2018/12/21
+ *
+ * @author codingapi
+ */
+@Mapper
+public interface DemoMapper extends BaseDemoMapper {
+}
